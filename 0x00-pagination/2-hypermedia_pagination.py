d7083353476ad579e8b2252797affbc9e1cc68e3
mouzerzo <mouzerzo@e2r12p11.1337.ma>
2024-05-20 18:06:39 +0100

    ne task

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Simple pagination"""
+"""Hypermedia pagination"""
+
 from typing import Tuple, List
 import csv
 import math
@@ -37,3 +38,19 @@ class Server:
         indexes = index_range(page, page_size)
         all_list = self.dataset()
         return all_list[indexes[0]:indexes[1]]
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """method get_hyper"""
+        data = self.get_page(page, page_size)
+        all_list = self.dataset()
+        total_data = len(all_list)
+        if (total_data / page_size) > page:
+            next_page = page + 1
+        else:
+            next_page = None
+        if page > 1:
+            prev_page = page - 1
+        else:
+            prev_page = None
+        total_pages = math.ceil(total_data / page_size)
+        return {'page_size': page_size, 'page': page, 'data': data, 'next_page': next_page, 'prev_page': prev_page, 'total_pages': int(total_pages)}
