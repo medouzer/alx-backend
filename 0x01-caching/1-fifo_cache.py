07ff3e56bd220b54658885d32f7c699fa07028dd
mouzerzo <mouzerzo@e2r11p9.1337.ma>
2024-05-23 02:41:12 +0100

    fix error

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -28,6 +28,4 @@ class FIFOCache(BaseCaching):
 
     def get(self, key):
         """ Get an item by key"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
