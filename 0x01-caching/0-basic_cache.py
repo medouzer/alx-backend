18199e3c711834e78037cf2f9dd2b4854f3f5a0d
mouzerzo <mouzerzo@e2r11p9.1337.ma>
2024-05-23 02:39:13 +0100

    fix error

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,9 +8,8 @@ class BasicCache(BaseCaching):
     """class BasicCache"""
     def put(self, key, item):
         """ Add an item in the cache"""
-        if key is None and item is None:
-            return
-        self.cache_data[key] = item
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key"""
