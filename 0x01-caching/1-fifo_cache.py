93c65eeb8f62b9ab7c37a07f232a3257e0332200
mouzerzo <mouzerzo@e2r11p9.1337.ma>
2024-05-23 02:41:51 +0100

    fix error

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,7 +15,7 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """ Add an item in the cache"""
-        if key is None and item is None:
+        if key is None or item is None:
             return
 
         if key in self.cache_data:
@@ -28,4 +28,6 @@ class FIFOCache(BaseCaching):
 
     def get(self, key):
         """ Get an item by key"""
-        return self.cache_data.get(key, None)
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
