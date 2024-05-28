fc21b8f0f6fc29f9833679cb899425deaf4a0774
ouzerzou <mohamed.ouz2016@gmail.com>
2024-05-28 23:15:13 +0100

    fix the check

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -23,7 +23,7 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
     """return the index"""
     return render_template('3-index.html')
