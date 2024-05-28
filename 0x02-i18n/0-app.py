dc60fad966e464a59ce89b88a348e85b25a211d4
mouzerzo <mouzerzo@e1r11p11.1337.ma>
2024-05-28 15:15:21 +0100

    first task

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('0-index.html')
