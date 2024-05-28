a088b79a01a46703d2c24f67e94920fc165c1886
ouzerzou <mohamed.ouz2016@gmail.com>
2024-05-28 23:57:25 +0100

    fix errors

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -48,10 +48,11 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
-
-
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index() -> str:
     """return the index"""
     return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
