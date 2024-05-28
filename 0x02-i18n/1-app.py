e2f7ce7b40eefc263f12f008229f1b9c25038851
mouzerzo <mouzerzo@e1r11p11.1337.ma>
2024-05-28 20:55:45 +0100

    new things

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+from flask import request
+
+class Config:
+    """config class"""
+    LANGUAGES = ['en', 'fr']
+
+
+def get_locale():
+    """method get_local"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+app = Flask(__name__)
+babel = Babel(app, locale_selector=get_locale, timezone_selector="UTC")
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('0-index.html')
