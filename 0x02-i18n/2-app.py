6dea13edfe2d75a91cd2ea9c573274da845de925
mouzerzo <mouzerzo@e1r11p11.1337.ma>
2024-05-28 21:07:09 +0100

    task 3

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+from flask import request
+
+class Config:
+    """config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+def get_locale():
+    """method get_local"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+app = Flask(__name__)
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('1-index.html')
