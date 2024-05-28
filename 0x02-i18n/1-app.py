3f690d5c590feb7293511432dec65a2d96800cfe
mouzerzo <mouzerzo@e1r11p11.1337.ma>
2024-05-28 21:03:51 +0100

    fix Error

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,20 +6,19 @@ from flask import render_template
 from flask_babel import Babel
 from flask import request
 
+
 class Config:
     """config class"""
     LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
-def get_locale():
-    """method get_local"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
-babel = Babel(app, locale_selector=get_locale, timezone_selector="UTC")
-
+app.config.from_object(Config)
+babel = Babel(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('0-index.html')
+    return render_template('1-index.html')
